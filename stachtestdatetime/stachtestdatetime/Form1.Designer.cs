98a31bb10418bbe29e7ed8f350fe926cd2d32534
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2022-11-03 13:20:33 +0100

    Add files via upload

---
+++ stachtestdatetime/stachtestdatetime/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace stachtestdatetime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(43, 69);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(338, 69);
+            this.maskedTextBox1.Mask = "000000/000";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 1;
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(338, 136);
+            this.maskedTextBox2.Mask = "000000/0000";
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(97, 20);
+            this.maskedTextBox2.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Před";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Po";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 257);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ stachtestdatetime/stachtestdatetime/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stachtestdatetime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        DateTime treba;
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            
+                
+                treba = dateTimePicker1.Value;
+                treba = treba.Date;
+
+
+                int mesic = treba.Month;
+                int den = treba.Day;
+                string pomocna;
+                string pomocnaden;
+                if (mesic < 10)
+                {
+                    pomocna = mesic.ToString().Replace(mesic.ToString(), "0" + mesic.ToString());
+                }
+                else { pomocna = mesic.ToString(); }
+
+                if (den < 10)
+                {
+                    pomocnaden = den.ToString().Replace(den.ToString(), "0" + den.ToString());
+                }
+                else
+                { pomocnaden = den.ToString(); }
+                Random rng = new Random();
+                int poslednirandom = rng.Next(100, 999);
+                maskedTextBox1.Text = treba.Year.ToString().Substring(2) + pomocna + pomocnaden + poslednirandom.ToString();
+                int rodnecislo = int.Parse(maskedTextBox1.Text.Replace(".", ""));
+                
+
+                bool ukonceni = false;
+                long help = 0;
+            int naposled = 0;
+                Random last = new Random();
+                do
+                {
+                    naposled = last.Next(0, 10);
+                string konec = rodnecislo.ToString() + naposled;
+
+                    help = long.Parse(konec);
+                    if (help % 11 == 0)
+                    {
+                        ukonceni = true;
+
+                    }
+
+
+                } while (ukonceni == false);
+
+
+
+            maskedTextBox2.Text = maskedTextBox1.Text + naposled.ToString();
+            }
+
+        
+    }
+}
